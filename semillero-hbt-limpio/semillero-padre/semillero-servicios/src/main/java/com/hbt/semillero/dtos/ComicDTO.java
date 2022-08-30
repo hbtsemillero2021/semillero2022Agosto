8b18fc9528e50ae5046b1d290c35ab8be840a2ac
“DiegoAlvarez” <diego.alvarez@evertecinc.com>
2022-08-29 21:00:02 -0500

    sesion 8 JPA II

--- semillero-hbt-limpio/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/dtos/ComicDTO.java
+++ semillero-hbt-limpio/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/dtos/ComicDTO.java
@@ -1,94 +1,39 @@
-package com.hbt.semillero.entity;
+package com.hbt.semillero.dtos;
 
-
-
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
 
 import com.hbt.semillero.enums.EstadoEnum;
 import com.hbt.semillero.enums.TematicaEnum;
 
-/**
- * <b>Descripción:<b> Entidad que contiene la informacion de la tabla
- * comic en la base de datos de semillero
- * <b>Caso de Uso:<b> SEMILLERO2022
- * @author Diego Fernando Alvarez Silva
- * @version 1.0
- */
-@Entity
-@Table(name = "COMIC")
-public class Comic implements Serializable {
+public class ComicDTO {
 
-	/**
-	 * Serializar es pasar un Objeto a un array de bytes y viceversa. Atributo que
-	 * determina serialVersionUID es el id único que identifica una clase cuando lo
-	 * serializamos. ;ediante este id podemos identificar el objeto convertido en un
-	 * array de bytes.
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@SequenceGenerator(allocationSize = 1, name = "COMIC_SCID_GENERATOR", sequenceName = "SEQ_COMIC")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "COMIC_SCID_GENERATOR")
-	@Column(name = "SCID")
 	private Long id;
 	
-	@Column(name = "SCNOMBRE")
 	private String nombre;
 	
-	@Column(name = "SCEDITORIAL")
 	private String editorial;
 	
-	@Column(name = "SCTEMATICA")
-	@Enumerated(value = EnumType.STRING)
 	private TematicaEnum tematicaEnum;
 	
-	@Column(name = "SCCOLECCION")
 	private String coleccion;
 	
-	@Column(name = "SCNUMEROPAGINAS")
 	private Integer numeroPaginas;
 	
-	@Column(name = "SCPRECIO")	
 	private BigDecimal precio;
 	
-	@Column(name = "SCAUTORES")
 	private String autores;
 
-	@Column(name = "SCCOLOR")
 	private Boolean color;
 	
-	@Column(name = "SCFECHA_VENTA")
 	private LocalDate fechaVenta;
 	
-	@Column(name = "SCESTADO")
-	@Enumerated(value = EnumType.STRING)
 	private EstadoEnum estadoEnum;
-
-	@Column(name = "SCCANTIDAD")
+	
 	private Long cantidad;
 
 	/**
-	 * Constructor de la clase.
-	 */
-	public Comic() {
-
-	}
-
-	/**
 	 * Metodo encargado de retornar el valor del atributo id
-	 * 
 	 * @return El id asociado a la clase
 	 */
 	public Long getId() {
@@ -97,7 +42,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo id
-	 * 
 	 * @param id El nuevo id a modificar.
 	 */
 	public void setId(Long id) {
@@ -106,17 +50,14 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo nombre
-	 * 
 	 * @return El nombre asociado a la clase
 	 */
-
 	public String getNombre() {
 		return nombre;
 	}
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo nombre
-	 * 
 	 * @param nombre El nuevo nombre a modificar.
 	 */
 	public void setNombre(String nombre) {
@@ -125,17 +66,14 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo editorial
-	 * 
 	 * @return El editorial asociado a la clase
 	 */
-	
 	public String getEditorial() {
 		return editorial;
 	}
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo editorial
-	 * 
 	 * @param editorial El nuevo editorial a modificar.
 	 */
 	public void setEditorial(String editorial) {
@@ -143,18 +81,16 @@ public class Comic implements Serializable {
 	}
 
 	/**
-	 * Metodo encargado de retornar el valor del atributo tematica
-	 * 
-	 * @return El tematica asociado a la clase
+	 * Metodo encargado de retornar el valor del atributo tematicaEnum
+	 * @return El tematicaEnum asociado a la clase
 	 */
 	public TematicaEnum getTematicaEnum() {
 		return tematicaEnum;
 	}
 
 	/**
-	 * Metodo encargado de modificar el valor del atributo tematica
-	 * 
-	 * @param tematica El nuevo tematica a modificar.
+	 * Metodo encargado de modificar el valor del atributo tematicaEnum
+	 * @param tematicaEnum El nuevo tematicaEnum a modificar.
 	 */
 	public void setTematicaEnum(TematicaEnum tematicaEnum) {
 		this.tematicaEnum = tematicaEnum;
@@ -162,7 +98,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo coleccion
-	 * 
 	 * @return El coleccion asociado a la clase
 	 */
 	public String getColeccion() {
@@ -171,7 +106,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo coleccion
-	 * 
 	 * @param coleccion El nuevo coleccion a modificar.
 	 */
 	public void setColeccion(String coleccion) {
@@ -180,7 +114,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo numeroPaginas
-	 * 
 	 * @return El numeroPaginas asociado a la clase
 	 */
 	public Integer getNumeroPaginas() {
@@ -189,7 +122,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo numeroPaginas
-	 * 
 	 * @param numeroPaginas El nuevo numeroPaginas a modificar.
 	 */
 	public void setNumeroPaginas(Integer numeroPaginas) {
@@ -198,7 +130,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo precio
-	 * 
 	 * @return El precio asociado a la clase
 	 */
 	public BigDecimal getPrecio() {
@@ -207,7 +138,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo precio
-	 * 
 	 * @param precio El nuevo precio a modificar.
 	 */
 	public void setPrecio(BigDecimal precio) {
@@ -216,7 +146,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo autores
-	 * 
 	 * @return El autores asociado a la clase
 	 */
 	public String getAutores() {
@@ -225,7 +154,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo autores
-	 * 
 	 * @param autores El nuevo autores a modificar.
 	 */
 	public void setAutores(String autores) {
@@ -234,7 +162,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo color
-	 * 
 	 * @return El color asociado a la clase
 	 */
 	public Boolean getColor() {
@@ -243,7 +170,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo color
-	 * 
 	 * @param color El nuevo color a modificar.
 	 */
 	public void setColor(Boolean color) {
@@ -252,17 +178,14 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo fechaVenta
-	 * 
 	 * @return El fechaVenta asociado a la clase
 	 */
-	
 	public LocalDate getFechaVenta() {
 		return fechaVenta;
 	}
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo fechaVenta
-	 * 
 	 * @param fechaVenta El nuevo fechaVenta a modificar.
 	 */
 	public void setFechaVenta(LocalDate fechaVenta) {
@@ -270,18 +193,16 @@ public class Comic implements Serializable {
 	}
 
 	/**
-	 * Metodo encargado de retornar el valor del atributo estado
-	 * 
-	 * @return El estado asociado a la clase
+	 * Metodo encargado de retornar el valor del atributo estadoEnum
+	 * @return El estadoEnum asociado a la clase
 	 */
 	public EstadoEnum getEstadoEnum() {
 		return estadoEnum;
 	}
 
 	/**
-	 * Metodo encargado de modificar el valor del atributo estado
-	 * 
-	 * @param estado El nuevo estado a modificar.
+	 * Metodo encargado de modificar el valor del atributo estadoEnum
+	 * @param estadoEnum El nuevo estadoEnum a modificar.
 	 */
 	public void setEstadoEnum(EstadoEnum estadoEnum) {
 		this.estadoEnum = estadoEnum;
@@ -289,7 +210,6 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de retornar el valor del atributo cantidad
-	 * 
 	 * @return El cantidad asociado a la clase
 	 */
 	public Long getCantidad() {
@@ -298,22 +218,9 @@ public class Comic implements Serializable {
 
 	/**
 	 * Metodo encargado de modificar el valor del atributo cantidad
-	 * 
 	 * @param cantidad El nuevo cantidad a modificar.
 	 */
 	public void setCantidad(Long cantidad) {
 		this.cantidad = cantidad;
-	}
-
-	/**
-	 * @see java.lang.Object#toString() Metodo que permite asociar al objeto un
-	 *      texto representativo
-	 */
-	@Override
-	public String toString() {
-		return "Comic [id=" + id + ", nombre=" + nombre + ", editorial=" + editorial + ", tematica=" + tematicaEnum
-				+ ", coleccion=" + coleccion + ", numeroPaginas=" + numeroPaginas + ", precio=" + precio + ", autores="
-				+ autores + ", color=" + color + ", fechaVenta=" + fechaVenta + ", estado=" + estadoEnum + ", cantidad="
-				+ cantidad + "]";
 	}
 }
--- semillero-hbt-limpio/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/rest/SemilleroJPQLRest.java
+++ semillero-hbt-limpio/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/rest/SemilleroJPQLRest.java
@@ -3,12 +3,15 @@
  */
 package com.hbt.semillero.rest;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -20,6 +23,8 @@ import javax.ws.rs.core.MediaType;
 import org.apache.log4j.Logger;
 
 import com.hbt.semillero.entity.Comic;
+import com.hbt.semillero.enums.EstadoEnum;
+import com.hbt.semillero.enums.TematicaEnum;
 
 /**
  * <b>Descripción:<b> Clase que determina
@@ -36,14 +41,15 @@ public class SemilleroJPQLRest {
 	 * Constante que contendra el log de la clase AritmeticaTest
 	 */
 	private final static Logger LOG = Logger.getLogger(SemilleroJPQLRest.class);
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@GET
 	@Path("/test")
 	@Produces(MediaType.APPLICATION_JSON)
 	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	@SuppressWarnings("unchecked")
 	public String generarOperacionComic() {
 		LOG.info("Se ejecuta generarOperacionComic()");
 		Comic comic = null;
@@ -53,23 +59,80 @@ public class SemilleroJPQLRest {
 			// SELECT * FROM COMIC WHERE ID = 15;
 			comic = em.find(Comic.class, 15L);
 			LOG.info("DATA COMIC" + comic.toString());
-			
+
+			//Consults en JPQL para obtener un comic con el id 15 pero quemado o hardcodeado haciendo uso del metodo getSingleResult
 			String consulta = "SELECT c FROM Comic c WHERE c.id = 15 ";
 			Query queryUnComic = em.createQuery(consulta);
 			comic = (Comic) queryUnComic.getSingleResult();
-			
-			
-		} catch (NonUniqueResultException e) {
-			LOG.error("SE HA PRESENTADO UN ERROR TECNICO" + e.getMessage());
+
+
+			//Consulta en JPQL para obtener un comic con el id 6 tematicaenum y color haciendo uso del metodo getSingleResult y setParameter
+			String consultaDos = "SELECT cm FROM Comic cm "
+					+ " WHERE cm.id = 15 "
+					+ " AND cm.tematicaEnum = 'FANTASTICO'"
+					+ " AND cm.color = 0 "
+					+ " AND cm.estadoEnum = 'ACTIVO' " ;
+			Query queryUnComicDos = em.createQuery(consultaDos);			
+			comic = (Comic) queryUnComicDos.getSingleResult();
+
+
+			String consultaTres = "SELECT cm FROM Comic cm "
+					+ " WHERE cm.id = :idComic "
+					+ " AND cm.tematicaEnum = :tematicaEnum "
+					+ " AND cm.color = :color "
+					+ " AND cm.estadoEnum = :estado " ;
+			Query queryUnComicTres = em.createQuery(consultaTres);
+			queryUnComicTres.setParameter("idComic", comic.getId());
+			queryUnComicTres.setParameter("tematicaEnum", TematicaEnum.FANTASTICO);
+			queryUnComicTres.setParameter("color", Boolean.FALSE);
+			queryUnComicTres.setParameter("estado", EstadoEnum.ACTIVO);
+			comic = (Comic) queryUnComicTres.getSingleResult();
+
+			// Query que genera una exception de tipo NoResultException debido a que la consulta no retorna nada
+			//			String consultaCuarto = "SELECT cm FROM Comic cm WHERE cm.id = :idComic "
+			//					+ " AND cm.tematicaEnum = :tematicaEnum AND cm.color = :colorComic ";
+			//			Query queryUnComicCuatro = em.createQuery(consultaCuarto);
+			//			queryUnComicCuatro.setParameter("idComic", comic.getId());
+			//			queryUnComicCuatro.setParameter("tematicaEnum", TematicaEnum.HUMORISTICO);
+			//			queryUnComicCuatro.setParameter("colorComic", Boolean.TRUE);
+			//			comic = (Comic) queryUnComicCuatro.getSingleResult();
+
+			//Traer los comics filtrando por tematica y color como lista
+			String consultaListaComics = "SELECT cm FROM Comic cm WHERE cm.tematicaEnum = :tematicaEnum"
+					+ " AND cm.color = :colorComic ";
+			Query queryListComics = em.createQuery(consultaListaComics);
+			queryListComics.setParameter("tematicaEnum", TematicaEnum.FANTASTICO);
+			queryListComics.setParameter("colorComic", Boolean.FALSE);
+			List<Comic> listComics = queryListComics.getResultList();
+
+			for (Comic comicList : listComics) {
+				LOG.info("DATA COMIC" + comicList.toString());
+			}
+
+			// Query que genera una exception de tipo NonUniqueResultException debido a que la consulta retorna mas de 1 registro
+			String consultaListaNonUnique = "SELECT cm FROM Comic cm WHERE cm.tematicaEnum = :tematicaEnum"
+					+ " AND cm.color = :colorComic ";
+			Query queryNonUnique = em.createQuery(consultaListaNonUnique);
+			queryNonUnique.setParameter("tematicaEnum", TematicaEnum.FANTASTICO);
+			queryNonUnique.setParameter("colorComic", Boolean.FALSE);
+			comic = (Comic) queryNonUnique.getSingleResult();
+			//List<Comic> listComics = queryListComics.getSingleResult();
+
+		} catch (NonUniqueResultException nure) {
+			LOG.error("Se ha presentado ducplicidad de datos con el id 15" + nure.getMessage());
+		} catch (NoResultException nre) {
+			LOG.error("No se encontro registro para el id 15" + nre.getMessage());
+		} catch (Exception e) {
+			LOG.error("Se ha presentado un error tecnico" + e.getMessage());
 		}
-		
+
 		LOG.info("Finaliza generarOperacionComic()");
 		return "";
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 }
